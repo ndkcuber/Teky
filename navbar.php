be36f0bb98651856d2f21db99a50da4a4564403a
Nguyen Dong Kha <lmaoboi1807@gmail.com>
2022-12-08 20:47:01 +0700

    asd

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <div class="navbar">
 	<ul>
 		<li><a href="index.php" class="item">HOME</a></li>
-		<li><a href="javascript:void(0)" class="item">PRODUCTS</a></li>
+		<li><a href="products.php" class="item">PRODUCTS</a></li>
 		<li><a href="javascript:void(0)" class="item">ABOUT</a></li>
 	</ul>
 </div>
---
+++ products.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Trang chủ</title>
+</head>
+<body>
+	<?php
+ 		include "navbar.php";
+	 ?>
+	<div class="container">
+		<h1>SẢN PHẨM CỦA CHÚNG TÔI</h1>
+		<div style="margin: 40px;">
+			<div class="box">
+				<div class="title"><h2>Sản phẩm 1</h2></div>
+				<div class="image"></div>
+				<div class="description"><p>Đây là một sản phẩm test</p></div>
+				<a href="javascript:void(0)">Xem thêm</a>	
+			</div>
+		</div>
+	</div>
+</body>
+</html>
