7ce847a28806310dbde9703beb44722b15fd5f37
ndkcuber13 <lmaoboi1807@gmail.com>
2022-12-08 19:17:22 +0700

    a

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Bài 6</title>
 </head>
 <body>
-	<a href="https://youtube.com/" target="_blank">Click this nigga</a>
+	<?php
+ 		include "navbar.php";
+	 ?>
+	<div class="container">
+		
+	</div>
 </body>
 </html>
