f901ce58a9f069b7fd84d6079c154429d3086acc
Nguyen Dong Kha <lmaoboi1807@gmail.com>
2022-12-08 21:21:21 +0700

    asd

--- navbar.php
+++ navbar.php
@@ -3,5 +3,17 @@
 		<li><a href="index.php" class="item">HOME</a></li>
 		<li><a href="products.php" class="item">PRODUCTS</a></li>
 		<li><a href="javascript:void(0)" class="item">ABOUT</a></li>
+		<?php 
+		session_start();
+			if (!isset($_SESSION['username'])) {
+				echo "		<div style=\"float: right;\">
+								<li><a href=\"login.php\" class=\"item\" style=\"margin-right: 10px;\">Login</a></li>
+								<li><a href=\"register.php\" class=\"item\" style=\"margin-right: 10px;\">Register</a></li>
+							</div>";
+			} else{
+				echo "		<div style=\"float: right;\">
+								<li><a href=\"profile.php\" class=\"item\" style=\"margin-right: 10px;\">".$_SESSION['username']."</a></li>";
+			}
+		?>
 	</ul>
 </div>
