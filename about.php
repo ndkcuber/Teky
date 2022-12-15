6b3a0d457be6355de7684ff1091044ed2efb9c60
ndkcuber13 <lmaoboi1807@gmail.com>
2022-12-15 19:26:01 +0700

    Update about.php

--- about.php
+++ about.php
@@ -10,20 +10,21 @@
 	<?php
  		include "navbar.php";
 	 ?>
-	<div class="container" style="max-width: 720px;">
+	<div class="container" style="margin-bottom: 20px;">
 		<h1 style="color: salmon;"></h1>
 		<div class="roundcard">
 			<h1>Về chúng tôi:</h1>
-			<div style="margin: 30px; text-align: left;">
-				<h1>Đây là gì?</h1>
-				<h2 style="color:grey;">Đây là một trang web thuộc sở hữu của Nguyễn Đông Kha, chuyên cung cấp các mặt hàng liên quan đến linh kiện, phụ kiện, đồ công nghệ,...</h2>
-				<h1>Liên hệ</h1>
-				<h2 style="color:grey">Hotline: 0969 696 6969</h2>
-				<h2 style="color:grey;">Gửi báo cáo, khiếu nại bên dưới</h2>
-				<a href="report.php" target="_blank" class="defaultbtn">Tạo đơn khiếu nại</a>
-				
+			<div style="margin: 30px;">
+				<div style="float: left; clear:both; text-align: left; max-width:60%;">
+					<h1>GIỚI THIỆU VỀ TEKY</h1>
+					<p>TEKY là Học viện sáng tạo công nghệ với chương trình giảng dạy STEAM (Science – Technology – Engineering – Art – Mathematics) theo chuẩn Mỹ đầu tiên tại Việt Nam dành cho trẻ em từ 4 đến 18 tuổi. Được thành lập vào tháng 6 năm 2016, TEKY quyết tâm thực hiện sứ mệnh mang đến cho thế hệ trẻ Việt Nam kiến thức toàn diện về STEAM, đặc biệt là các tư duy công nghệ, khoa học máy tính và kỹ năng thế kỷ 21 – 4Cs (Critical Thinking: Tư duy phản biện – Communication: Giao tiếp – Creativity: Sáng tạo – Collaboration: Làm việc nhóm). Các bộ môn giảng dạy tại Teky gồm: Lập trình và phát triển ứng dụng, Robotics Engineering, Công nghệ 3D và MultiMedia. Chúng tôi tin rằng trẻ em Việt Nam có cơ hội phát triển mạnh mẽ trong một nền kinh tế số và cần được trang bị sẵn sàng để trở thành những doanh nhân công nghệ trong tương lai. Tháng 11/2016, Teky là đơn vị đầu tiên tại Việt Nam gặt hái được 5 huy chương bạc trong cuộc thi lập trình quốc tế WeCode – bộ môn lập trình Scratch cho độ tuổi 6-9. Tháng 3/2017, Teky là 1 trong 10 dự án Ảnh hưởng Xã hội trên toàn bộ khu vực Đông Nam Á và dự án duy nhất tại Việt Nam, theo bình chọn của Chính phủ Úc thông qua sàng lọc 2000 dự án khác. Tháng 6/2017, Teky là 1 trong 3 dự án được đánh giá cao nhất trong sự kiện Women NextGen Entrepreneur diễn ra tại Thuỵ sĩ. Tính tới tháng 9/2017, Teky đã đem chương trình Hour of Code tới trên 3.000 học sinh toàn quốc. Trong 3 năm hoạt động, Teky đã vinh dự nhận được 5 giải thưởng Rice Bowl Startup Award cho các hạng mục: “Dự án đào tạo có ảnh hưởng xã hội tốt nhất” (2017), “Khởi nghiệp nhiều người bình chọn nhất” (2018), và “Dự án cống hiến cho xã hội tốt nhất” (2018), “Founder Of The Year” và “Best NewComer” (2019) Bà Đào Lan Hương – Chủ tịch HĐQT TEKY Holdings đã đại diện tham gia phái đoàn Việt Nam tham dự Hội nghị Thượng đỉnh về Kinh doanh và Đầu tư ASEAN (ASEAN BIS), nhận giải thưởng “Doanh nghiệp khởi nghiệp và đổi mới sáng tạo xuất sắc” tại Giải thưởng Doanh nghiệp Đông Nam Á 2019 (ABA). Ông Lê Quang Tuấn, Đồng sáng lập – Giám đốc Đào tạo TEKY tự hào nhận được Giải thưởng dành cho Nhà lãnh đạo Giáo dục công nghệ Xuất sắc nhất Châu Á tại Singapore vào giữa tháng 11.2019. Tháng 12/2019, Học viện Sáng tạo Công nghệ Teky được tổ chức World Economic Forum (WEF) bầu chọn là mô hình giáo dục toàn cầu tiêu biểu tại diễn đàn kinh tế Davos, Thụy Sĩ. Diễn đàn kinh tế Davos tổ chức hàng năm tại Thuỵ Sĩ là sự kiện về kinh tế lớn thế giới với sự tham dự của chính khách đến từ các nước phát triển, các tỷ phú và tầng lớp siêu giàu đến tìm cơ hội hợp tác, đầu tư. Học viên được đào tạo tại TEKY cũng đã tự tin chinh chiến và xuất sắc đạt thành tích cao tại các cuộc thi công nghệ quốc tế, như Chung kết cuộc thi World Robot Olympiad Vietnam; giành các huy chương và giải thưởng trong các cuộc thi sáng tạo công nghệ cho trẻ em như WeCode hay AppJamming Summit.</p>
+				</div>
+				<div style="float: right; margin: 40px;">
+					<img src="https://teky.edu.vn/static/web/upload/about-intro.png" >
+				</div>
 			</div>
 		</div>
+		<a href="report.php" target="_blank" class="defaultbtn">Liên hệ hỗ trợ</a>
 	</div>
 </body>
 </html>
