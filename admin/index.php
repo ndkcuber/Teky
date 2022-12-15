ece4032fc2cef5621d58858b349fb9bbe45e72be
ndkcuber13 <lmaoboi1807@gmail.com>
2022-12-15 18:43:35 +0700

    very cul

---
+++ admin/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Admin</title>
+</head>
+<body>
+	<div id="header">Header</div>
+	<div id="menu">Menu</div>
+	<div id="left">Left</div>
+	<div id="right">Right</div>
+	<div id="content">Content</div>
+	<div id="footer">
+		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/HTML5_logo_resized.svg/1200px-HTML5_logo_resized.svg.png" style="width: 140px; float: left;"><p>HTML là hypertext make up language, rất là đẹp trai omg omg omg real look at this cool looking language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language language </p>
+	</div>
+</body>
+</html>
