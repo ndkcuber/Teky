80d6bd4d587e1d4b781f8d0b886055b0b945bcd2
Nguyen Dong Kha <lmaoboi1807@gmail.com>
2022-12-22 19:03:55 +0700

    a
    a

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": ["#FF5733", "#F3B029", "#F3D129"]
+        "value": ["#D3F3D8", "#9DA99E", "#EDEAE2"]
       },
       "shape": {
         "type": ["polygon", "circle"],
@@ -61,11 +61,11 @@ particlesJS('particles-js',
         }
       },
       "line_linked": {
-        "enable": false,
+        "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#0F0F0F",
         "opacity": 0.4,
-        "width": 1
+        "width": 4
       },
       "move": {
         "enable": true,
