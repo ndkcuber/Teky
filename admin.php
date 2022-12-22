9f690253ff01d1098e9f4456a454750fc774c70e
Nguyen Dong Kha <lmaoboi1807@gmail.com>
2022-12-22 18:38:41 +0700

    asd
    asd

--- admin.php
+++ admin.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Đăng nhập</title>
+	<title>Trang quản lí</title>
 </head>
 <body>
 	<?php
@@ -24,9 +24,10 @@
 	 ?>
 	<div class="container" style="max-width: 720px;">
 		<div class="roundcard">
-			<h1>Trang cá nhân</h1>
+			<h1>Quản lí</h1>
 			<div style="margin: 30px; text-align: left;">
-
+				<a href="" target="_blank">Thêm sản phẩm</a>
+				<a href="" target="_blank">Quản lí đơn hàng</a>
 			</div>
 	
 		</div>
