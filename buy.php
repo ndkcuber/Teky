815f99a9749a4e407089e7e7c1bf2ed4aed37d1a
Nguyen Dong Kha <lmaoboi1807@gmail.com>
2022-12-27 08:47:27 +0700

    1
    23

--- buy.php
+++ buy.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Đăng nhập</title>
-	<div id="particles-js"></div>
-	<script type="text/javascript" src="particles.js"></script>
-	<script type="text/javascript" src="app.js"></script>
-</head>
-<body>
 	<?php
- 		include "navbar.php";
 		include "database.php";
 		if (!isset($_GET['id'])) {
 			header("Location: index.php");
@@ -21,30 +8,25 @@
 			$result = mysqli_query($conn,$sql);
 			$row = $result->fetch_assoc();
 			$product_stock = $row['product_stock'];
-			$new_stock = $product_stock-1;
-			$sql = "UPDATE `products` SET `product_stock`='".$new_stock."' WHERE `product_id`='".$_GET['id']."'";
-			$result = mysqli_query($conn,$sql);
+			if ($product_stock == "0") {
+				echo "<script>alert(\"Hết hàng!\")</script>";
+				echo "<script>window.close();</script>";
+			} else{
+				$new_stock = $product_stock-1;
+				$sql = "UPDATE `products` SET `product_stock`='".$new_stock."' WHERE `product_id`='".$_GET['id']."'";
+				$result = mysqli_query($conn,$sql);
+	
+				$user = $_SESSION['username'];
+				$product_name = $row['product_name'];
+				date_default_timezone_set('Asia/Ho_Chi_Minh');
+				$date = date('d-m-Y');
+	
+				$sql = "INSERT INTO `prodreport`(`user`, `productname`, `date`) VALUES ('".$user."','".$product_name."','".$date."')";
+				$result = mysqli_query($conn,$sql);
+				echo "<script>alert(\"Mua hàng thành công!\")</script>";
+				echo "<script>window.close();</script>";
+			}
 
 
-			$user = $_SESSION['username'];
-			$product_name = $row['product_name'];
-			date_default_timezone_set('Asia/Ho_Chi_Minh');
-			$date = date('d-m-Y');
-
-			$sql = "INSERT INTO `prodreport`(`user`, `productname`, `date`) VALUES ('".$user."','".$product_name."','".$date."')";
-			$result = mysqli_query($conn,$sql);
 			}
 	 ?>
-	<div class="container" style="max-width: 720px;">
-		<div class="roundcard">
-			<div style="margin: 30px; text-align: center;">
-				<?php 
-					echo "<script>alert(\"Mua hàng thành công!\")</script>";
-					echo "<script>window.close();</script>";
-				?>
-				<meta http-equiv="refresh" content="0; url=index.php" />
-			</div>
-		</div>
-	</div>
-</body>
-</html>
